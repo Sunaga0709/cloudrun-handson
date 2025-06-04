4f9793639fbb22043d77142f83730a313ab1af03
Sunaga0709 <tsunaga0709@gmail.com>
2025-06-04 23:08:36 +0900

    fix cloudsql setting

--- cloudsql.tf
+++ cloudsql.tf
@@ -16,27 +16,27 @@ resource "google_sql_database_instance" "main" {
     edition                     = "ENTERPRISE"
     availability_type           = "ZONAL"
     connector_enforcement       = "NOT_REQUIRED" # Proxy経由を強制
-    # data_cache_config {
-    #   data_cache_enabled = false
-    # }
-    # ip_configuration {
-    #   private_network = google_compute_network.vpc_network.id
-    #   ipv4_enabled    = false # Public IP
-    #   ssl_mode        = "ALLOW_UNENCRYPTED_AND_ENCRYPTED"
-    # }
-    # maintenance_window {
-    #   day  = 1 # Monday
-    #   hour = 4
-    # }
-    # insights_config {
-    #   query_insights_enabled = false
-    #   query_string_length    = 1024
-    # }
-    # password_validation_policy {
-    #   enable_password_policy = true
-    #   min_length             = 6
-    #   reuse_interval         = 5
-    # }
+    data_cache_config {
+      data_cache_enabled = false
+    }
+    ip_configuration {
+      private_network = google_compute_network.vpc_network.id
+      ipv4_enabled    = false # Public IP
+      ssl_mode        = "ALLOW_UNENCRYPTED_AND_ENCRYPTED"
+    }
+    maintenance_window {
+      day  = 1 # Monday
+      hour = 4
+    }
+    insights_config {
+      query_insights_enabled = false
+      query_string_length    = 1024
+    }
+    password_validation_policy {
+      enable_password_policy = true
+      min_length             = 6
+      reuse_interval         = 5
+    }
   }
 
   depends_on = [google_project_service.activate_apis]
